0e97cbef3b73b7b0716b5202d6af6606eb6ec6ad
spark1216 <142547222+spark1216@users.noreply.github.com>
2023-10-23 23:13:27 -0700

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,26 @@
+info = {
+   "Pronoun": "her", 
+   "Name": "Sonia",
+   "Full_Name": "Sonia Park",
+   "Intro": "Aspiring Data Scientist with a strong background in programming and statistics",
+   "About":"Hey there, I'm Sonia! My true passion revolves around turning data into meaningful insights that drive impactful decisions. My expertise covers a wide range of topics, including Machine Learning and Data Science.",
+   "Tableau":"Placeholder",
+   "Regression Project":"Placeholder",
+   "Deep Learning Project": "Placeholder",
+   "NLP Project": "Placeholder",
+   "City":"Los Angeles, California",
+   "Photo":"""<a href=\"https://www.linkedin.com/in/sonia-park-hi/\"><img src=\"https://ibb.co/ng2VxTZ" width=\"200\"   alt=\"Profile\" title=\"Profile\"></a>""",
+   "Email": "spark400@usc.edu"
+}
+
+embed_rss= {
+    'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
+        data-rss="https://medium.com/feed/@vicky-note"
+        data-maxcols="3" 
+        data-layout="grid"
+        data-poststyle="inline" 
+        data-readmore="Read the rest" 
+        data-buttonclass="btn btn-primary" 
+        data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""
+}
+
