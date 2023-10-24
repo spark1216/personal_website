509fe741b79801f3792a5c35b4b557e576d8994e
spark1216 <142547222+spark1216@users.noreply.github.com>
2023-10-23 23:38:07 -0700

    Update 2_Resume.py

--- pages/2_Resume.py
+++ pages/2_Resume.py
@@ -12,10 +12,7 @@ st.sidebar.markdown(info['Photo'],unsafe_allow_html=True)
 
 st.title("📝 Resume")
 
-st.write("[Click here if it's blocked by your browser](https://drive.google.com/file/d/14Nv3thLxYOmHb4VbqkyOmhH4xPosD0zV/view?usp=drive_link)")
+st.write("[Click here!](https://drive.google.com/file/d/14Nv3thLxYOmHb4VbqkyOmhH4xPosD0zV/view?usp=drive_link)")
 
-with open("images/resume.pdf","rb") as f:
-      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-      pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
-      st.markdown(pdf_display, unsafe_allow_html=True)
+
   
