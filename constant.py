7279e7160c406f96565ea2edaf3ba2baf30a8210
spark1216 <142547222+spark1216@users.noreply.github.com>
2023-10-24 21:10:13 -0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -10,7 +10,6 @@ info = {
    "NLP Project": "Placeholder",
    "City":"Los Angeles, California",
    "Photo": """<a href="https://www.linkedin.com/in/sonia-park-hi/">
-              <img src="https://drive.google.com/file/d/1PrpzZL4ISRMOnIYjC98xd_ejM0LuPx8Y/view?usp=sharing" width="200" alt="Profile" title="Profile">
           </a>""",
    "Email": "spark400@usc.edu"
 }
